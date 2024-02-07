eafd102141d1b1b498946646db96f0bca7c55f4a
indra <92654577+dekindrama@users.noreply.github.com>
2024-02-07 13:03:41 +0800

    change ui

--- src/components/public/Card.tsx
+++ src/components/public/Card.tsx
@@ -21,7 +21,7 @@ const Card = ({
           src={item.picture}
           alt={item.name}
           className="h-72 w-full object-cover"
-          defaultSrc="https://images.unsplash.com/photo-1612178537253-bccd437b730e?q=80&w=2677&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          defaultSrc="/no-image.png"
         />
       </div>
       <div className="flex items-center justify-between p-5">
